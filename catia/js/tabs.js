7f245b6758a0721dfed5438e2cad33ef90aaab87
Felkosh <38834961+Felkosh@users.noreply.github.com>
2018-12-04 19:38:07 +0300

    Add files via upload

--- catia/js/tabs.js
+++ catia/js/tabs.js
@@ -1,10 +1,31 @@
+
 (function($) {
+var currenttabs = 1;
+
 $(function() {
 
 	$('ul.tabs').delegate('li:not(.current)', 'click', function() {
+		currenttabs = $(this).attr('data');
 		$(this).addClass('current').siblings().removeClass('current')
 			.parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(300);
 	})
+	$('.next__tab').on('click', function() {
+		currenttabs++;
+		if (currenttabs ==4 ) {
+			currenttabs = 1;
+		}
+		$('ul.tabs li[data="'+currenttabs+'"]').addClass('current').siblings().removeClass('current')
+			.parents('div.section').find('div.box').hide().eq($('ul.tabs li[data="'+currenttabs+'"]').index()).fadeIn(300);
 
+	})
+	$('.prev__tab').on('click', function() {
+		currenttabs--;
+		if (currenttabs ==0 ) {
+			currenttabs = 3;
+		}
+		$('ul.tabs li[data="'+currenttabs+'"]').addClass('current').siblings().removeClass('current')
+			.parents('div.section').find('div.box').hide().eq($('ul.tabs li[data="'+currenttabs+'"]').index()).fadeIn(300);
+
+	})
 })
 })(jQuery)

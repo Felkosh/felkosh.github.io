275dab577178a25eece3d0201c38b3f589ef3d1f
Felkosh <38834961+Felkosh@users.noreply.github.com>
2019-04-15 10:02:08 +0300

    Add files via upload

---
+++ videfontoutube/js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'Hhm6AJ3yoo8', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});

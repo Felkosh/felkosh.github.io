8b615c47ea7aeed2781edeb70b2b1bef8d53d4c0
Felkosh <38834961+Felkosh@users.noreply.github.com>
2019-08-20 16:23:48 +0300

    Add files via upload

--- мусор/js/main.js
+++ мусор/js/main.js
@@ -1,5 +1,10 @@
-addLoadEvent(
-    //call plugins after DOM ready
-
-    $( '.svg' ).svgToInline()
-);
+(function($) {
+$(function() {
+ 
+    $('ul.tabs').delegate('li:not(.current)', 'click', function() {
+        $(this).addClass('current').siblings().removeClass('current')
+            .parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(150);
+    })
+ 
+})
+})(jQuery)
